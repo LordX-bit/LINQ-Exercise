6338dc628cf5ec432bc466cb5d2c9274809b81b0
jose valdez <josezepedavaldez30@gmail.com>
2023-04-25 13:52:37 -0700

    initial commit

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -2,9 +2,23 @@
 {
     public class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            List<string> videoGamesNames = new List<string>() 
+            {"Warzone", "Fortnite", "GrandTheft Auto"};
+            videoGamesNames.Add("Ben Ten");
+            videoGamesNames.Add("Call Of Duty");
+
+            var ordererdNames = videoGamesNames.OrderByDescending(name => name.Length);
+            foreach (var games in ordererdNames)
+            {
+                Console.WriteLine(games);
+            }
+
+
+           // Console.WriteLine("Hello, World!");
         }
     }
 }
